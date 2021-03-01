f6659c30efc219da8213ed5b6462e3759ae289f5
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 22:01:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
 
+  attr_accessor :backers
   attr_reader :title
 
   def initialize(title)
-    this.title = title
-    
+    @title = title
+    @backers = []
   end
 
+  def add_backer(backer)
+    self.backers << backer
+  end
 end
